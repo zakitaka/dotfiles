9eb056977e207649006545ea0f51320da8b29e0d
Yamazaki Takashi <yamazaki_takashi@me.com>
2015-03-13 00:42:58 +0900

    oh-my-zsh設定用のスクリプトを更新
    カスタムのaliasファイルを読み込むようにするため

--- init_zsh-theme.sh
+++ init_zsh-theme.sh
@@ -3,3 +3,6 @@
 ### ディレクトリの絶対パス
 abspath=$(cd $(dirname $0) && pwd)
 ln -fsv $abspath/_custom.zsh-theme ~/.oh-my-zsh/themes/custom.zsh-theme
+
+### カスタムのaliasを設定
+ln -fsv $abspath/custom_alias.zsh ~/.oh-my-zsh/custom/custom_alias.zsh
